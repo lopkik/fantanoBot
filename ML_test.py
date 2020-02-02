d60ab9993db08584119f45e734f1ebc4cf7817cf
Justin Ji <justij4@uci.edu>
2020-02-02 03:54:42 -0800

    finished model should implement genre

---
+++ ML_test.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import csv
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from collections import defaultdict
+import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+import json
+from json.decoder import JSONDecodeError
+
+client_id = "62ea08ff57004c409b395aeef8fc23cd"
+client_secret = "6acf6c898c80405f8f4434b71755cf90"
+client_redirect_uri = 'http://localhost:8888'
+client_cred = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
+sp = spotipy.Spotify(client_credentials_manager = client_cred)
+
+fantano_file = './feature_ratings.csv'
+fantano_data = pd.read_csv(fantano_file)
+y = fantano_data.Score
+# print('y', type(y))
+
+fantano_features = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']
+X = fantano_data[fantano_features]
+# print('X', type(X))
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+# fantano_model = DecisionTreeRegressor(random_state=1)
+# fantano_model.fit(train_X, train_y)
+
+fantano_model = RandomForestRegressor(random_state=1, max_leaf_nodes=100)
+fantano_model.fit(X, y)
+# fantano_preds = fantano_model.predict(val_X)
+# print("MAE:", mean_absolute_error(val_y, fantano_preds))
+
+#here
+while True:
+    new_album = input("Enter an album ('get me out plz' to quit): ")
+    if new_album == 'get me out plz':
+        break
+
+    album_id = sp.search(new_album, type = 'album', limit = 1)['albums']['items'][0]['id']
+
+    album_file = './albums_rating.csv'
+
+    csv_col = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']
+    # album_csv = open(album_file, 'w', encoding = 'utf8')
+
+
+    with open(album_file, 'w', encoding = 'utf8') as f:
+        f.write("%s,%s,%s,%s,%s,%s,%s\n"%(csv_col[0], csv_col[1], csv_col[2], csv_col[3], csv_col[4], csv_col[5], csv_col[6]))
+        csv_f = csv.reader(f)
+        temp_tracklist = []
+        dictAvgFeat = defaultdict(list)
+
+        for tracknum in range(len(sp.album(album_id)['tracks']['items'])):
+            temp_tracklist.append(sp.album(album_id)['tracks']['items'][tracknum]['id'])
+        for track in sp.audio_features(temp_tracklist):
+            if track is None:
+                continue
+            for feature in track:
+                if feature in csv_col:
+                    dictAvgFeat[feature].append(track[feature])
+        dictAvgFeat = {x:sum(y)/len(y) for x, y in dictAvgFeat.items()}
+        f.write("%s,%s,%s,%s,%s,%s,%s\n"%(dictAvgFeat['acousticness'],dictAvgFeat['danceability'],dictAvgFeat['energy'],dictAvgFeat['instrumentalness'],dictAvgFeat['liveness'],dictAvgFeat['speechiness'],dictAvgFeat['valence']))
+
+    print(album_file)
+    input_data = pd.read_csv(album_file)
+    input_X = input_data[fantano_features]
+    pred_score = fantano_model.predict(input_X)
+
+    print("WOW, ", pred_score)
--- spotify_data.py
+++ spotify_data.py
@@ -24,8 +24,6 @@ sp = spotipy.Spotify(client_credentials_manager = client_cred)
 #print(sp.album(result['tracks']['items'][0]['album']['id'])['genres'])
 #print(sp.audio_features([sp.album(result['tracks']['items'][0]['album']['id'])['tracks']['items'][x]['id'] for x in range(sp.album(result['tracks']['items'][0]['album']['id'])['total_tracks'])]))
 
-name = "Lover" #input
-result = sp.search(name, type="album", limit=1)
 # for tracks in sp.audio_features([sp.album(result['tracks']['items'][0]['album']['id'])['tracks']['items'][x]['id'] for x in range(sp.album(result['tracks']['items'][0]['album']['id'])['total_tracks'])]):
 #     for features in tracks:
 #         if features not in dictAvgFeat and features in csv_columns:
@@ -38,13 +36,18 @@ result = sp.search(name, type="album", limit=1)
 #     writer.writeheader()
 #     writer.writerow(dictAvgFeat)
 
+name = "Lover" #input
+result = sp.search(name, type="album", limit=1)
+
 
 fantano_file = './fantano_ratings.csv'
-csv_file = "AverageAudioFeature.csv"
+# csv_file = "AverageAudioFeature.csv"
 csv_col = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness', 'valence']
 
-feature_csv = open('feature_rating.csv', 'w', encoding = 'utf8')
-feature_csv.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"%("Artist","Album","Score", csv_col[0], csv_col[1], csv_col[2], csv_col[3], csv_col[4], csv_col[5], csv_col[6]))
+result_csv = open('featureAndGenre_ratings.csv', 'w', encoding = 'utf8')
+avgGenre_csv = open('avg_genre.csv', encoding='utf8')
+
+result_csv.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"%("Artist","Album","Score", csv_col[0], csv_col[1], csv_col[2], csv_col[3], csv_col[4], csv_col[5], csv_col[6], "genre"))
 
 with open(fantano_file, encoding = 'utf8') as f:
     csv_f = csv.reader(f)
@@ -53,11 +56,11 @@ with open(fantano_file, encoding = 'utf8') as f:
             #collect the extracted info and put it into the table
             #same row as the album (not necessarily the same file)
             #album = album object (however that happens)
-            print(entry[1])
+            # print(entry[1])
             if len(sp.search(entry[1], type = 'album', limit = 1)['albums']['items']) == 0:
                 continue
             album_id = sp.search(entry[1], type = 'album', limit = 1)['albums']['items'][0]['id']
-            # hyperS~~~~~
+
             #for track in album (this will probably be something in the json lol):
             #   extract audio features into some variables
             temp_tracklist = []
@@ -71,9 +74,9 @@ with open(fantano_file, encoding = 'utf8') as f:
                 for feature in track:
                     if feature in csv_col:
                         dictAvgFeat[feature].append(track[feature])
-            print(dictAvgFeat)
+            # print(dictAvgFeat)
             dictAvgFeat = {x:sum(y)/len(y) for x, y in dictAvgFeat.items()}
-            print(dictAvgFeat)
+            # print(dictAvgFeat)
 
 
             #after calculating audio features
@@ -81,3 +84,4 @@ with open(fantano_file, encoding = 'utf8') as f:
             feature_csv.write("%s,%s,%s"%(entry[0],entry[1],entry[2]))
             feature_csv.write(",%s,%s,%s,%s,%s,%s,%s\n"%(dictAvgFeat['acousticness'],dictAvgFeat['danceability'],dictAvgFeat['energy'],dictAvgFeat['instrumentalness'],dictAvgFeat['liveness'],dictAvgFeat['speechiness'],dictAvgFeat['valence']))
 
+            # this gets the avg genre score from artists
